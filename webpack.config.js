7e4df3ebe9b53a4695497c8ad46042986afe3940
JDinAbox <jd@angelstosaints.com>
2021-02-06 04:57:46 -0500

    Update v/25.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -7,61 +7,62 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const TerserPlugin = require( 'terser-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	devtool: 'source-map',
 	performance: { hints: false },
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
 
 	output: {
 		// The name under which the editor will be exported.
 		library: 'MarkdownEditor',
 
-		path: path.resolve( __dirname, 'build' ),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
 		libraryExport: 'default'
 	},
 
 	optimization: {
+		minimize: true,
 		minimizer: [
-			new TerserPlugin( {
-				sourceMap: true,
+			new TerserPlugin({
+				parallel: true,
 				terserOptions: {
-					output: {
+					format: {
 						// Preserve CKEditor 5 license comments.
 						comments: /^!/
 					}
 				},
 				extractComments: false
-			} )
+			}),
 		]
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
+		new CKEditorWebpackPlugin({
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
 			language: 'en',
 			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
+		}),
+		new webpack.BannerPlugin({
 			banner: bundler.getLicenseBanner(),
 			raw: true
-		} )
+		})
 	],
 
 	module: {
 		rules: [
 			{
 				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				use: ['raw-loader']
 			},
 			{
 				test: /\.css$/,
